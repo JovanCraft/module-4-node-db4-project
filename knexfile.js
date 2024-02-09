7acd00db91553c48c9ab324421a9e9124546e02d
jovan <jovancraft@gmail.com>
2024-02-09 14:46:55 -0600

    changed the look of my knexfile to add testing and possible production

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,33 @@
+const sharedConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations"
+    },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+  },
+};
+
 module.exports = {
     development: {
-        client: 'sqlite3',
-        useNullAsDefault: true,
+        ...sharedConfig,
         connection: {
             filename: './data/recipe_book.db3',
         },
-        migrations: {
-            directory: './data/migrations'
-        },
         seeds: {
-            directory: './data/seeds'
+            directory: './data/seeds',
         },
-        pool: {
-            afterCreate: (conn, done) => {
-                conn.run('PRAGMA foreign_keys = ON', done);
-            }
-        }
+    },
+    testing: {
+        ...sharedConfig,
+        connection: {
+            filename: './data/recipe_book.test.db3',
+        },
+    },
+    production: {
+
     }
 }
