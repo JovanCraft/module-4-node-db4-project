a6a0b532cd189dd874203ff945890d833217b9dd
jovan <jovancraft@gmail.com>
2024-02-09 14:21:21 -0600

    wip

--- api/router.js
+++ api/router.js
@@ -3,7 +3,9 @@ const { getRecipeById } = require('./model')// eslint-disable-line
 const router = express.Router();
 
 
-
+router.use('*', (req, res, next) => {// eslint-disable-line
+    res.json({ api: 'up Up UP!!' })
+})
 
 
 
